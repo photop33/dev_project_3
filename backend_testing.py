29403ca19d4cf682530ae92f6fbbf1a9aca92208
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-02-26 08:27:08 +0200

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -10,22 +10,10 @@ host, port, sqlPort, user, passwd, db = '127.0.0.1', 5000, 3306, 'root', '123456
 try:
     res = requests.post(f'http://{host}:{port}/users/{id}', json={"user_name": f'{user_name}'})
     print("post response -", res.json())
-    print("host",host)
 
     res = requests.get(f'http://{host}:{port}/users/{id}')
     print("get response -", res.json())
-    
-    # Establishing a connection to DB 
-    conn = pymysql.connect(host=host, port=sqlPort, user=user, passwd=passwd, db=db,
-            cursorclass=pymysql.cursors.DictCursor)
-    conn.autocommit(True)
-    # Getting a cursor from Database
-    cursor = conn.cursor()
-    
-    cursor.execute(f"SELECT * FROM {db}.users WHERE user_id = {id};")
-    data = cursor.fetchall()
-    user_name = data[0]['user_name']
-    print("DB response -", user_name)
+
 except Exception as e:
     print("test failed",e)
     raise Exception(e)
